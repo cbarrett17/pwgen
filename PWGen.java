ae06414bab879f6c18e5584fc6900658d55c10df
Daniel Paxton <dpaxton18@georgefox.edu>
2021-11-11 22:16:49 -0800

    Declare array lists containing numbers, letters, and symbols.

--- PWGen.java
+++ PWGen.java
@@ -4,6 +4,15 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 
 public class PWGen {
+
+    private static final String[] UPPERCASE_ALPHA = {"A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private static final String[] LOWERCASE_ALPHA = {"a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    private static final String[] NUMERIC = {"0","1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private static final String[] SYMBOL = {"~", "!", "@", "#", "$","%", "^", "&", "*", "(", ")", "_", "+", "`", "-",
+            "=", "{", "}", "[", "]", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "\\", "?"};
+
     public static void main(String[] args) {
         Options options;
 
