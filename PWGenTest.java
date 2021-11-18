41dd6a7de86703b7b2becceaad10b87fc1a0f16c
Samuel Lorenz <slorenz19@georgefox.edu>
2021-11-18 11:45:36 -0800

    Got necessary imports for testing.

--- PWGenTest.java
+++ PWGenTest.java
@@ -1,7 +1,14 @@
+import static org.hamcrest.Matchers.*;
+import org.junit.*;
+import org.junit.rules.*;
+import org.junit.contrib.java.lang.system.*;
+import static org.junit.Assert.*;
+
+
 public class PWGenTest
 {
     public static void main(String[] args)
     {
-        //TODO JUnit unit test implementation
+
     }
 }
