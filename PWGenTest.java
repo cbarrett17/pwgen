fb599914dab8d8fdb034157785739570702ed287
Daniel Paxton <dpaxton18@georgefox.edu>
2021-11-20 20:53:06 -0800

    Added tests that check each argument works.

--- PWGenTest.java
+++ PWGenTest.java
@@ -4,17 +4,60 @@ import org.junit.rules.*;
 import org.junit.contrib.java.lang.system.*;
 import static org.junit.Assert.*;
 
-
-
 public class PWGenTest
 {
+    /**
+     * Verify commands are displayed when -h is provided.
+     */
     @Test
-    public void test_test()
+    public void TestPWGen_1()
+    {
+        final String[] args = {"-h"};
+
+        PWGen.main(args);
+    }
+
+    /**
+     * Verify the length and that all possible characters are added when only -n is provided.
+     */
+    @Test
+    public void TestPWGen_2()
     {
         final String[] args = {"-n", "8"};
 
         PWGen.main(args);
     }
 
+    /**
+     * Verify only letters are added when -a and -n is provided.
+     */
+    @Test
+    public void TestPWGen_3()
+    {
+        final String[] args = {"-a", "-n", "8"};
 
+        PWGen.main(args);
+    }
+
+    /**
+     * Verify only numbers are added when -d and -n is provided.
+     */
+    @Test
+    public void TestPWGen_4()
+    {
+        final String[] args = {"-d", "-n", "8"};
+
+        PWGen.main(args);
+    }
+
+    /**
+     * Verify only symbols are added when -s and -n is provided.
+     */
+    @Test
+    public void TestPWGen_5()
+    {
+        final String[] args = {"--symbol", "-n", "8"};
+
+        PWGen.main(args);
+    }
 }
